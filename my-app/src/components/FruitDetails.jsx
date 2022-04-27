a6e4e265d32374f0dc00a9b91d080eb6474c7921
aandersonozhoh <89994451+aandersonozhoh@users.noreply.github.com>
2022-04-27 00:02:31 -0500

    Removed nutrition component & more unnecessary code.

--- my-app/src/components/FruitDetails.jsx
+++ my-app/src/components/FruitDetails.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { BASE_URL, API_KEY } from '../globals'
+import { BASE_URL } from '../globals'
 
 const FruitDetails = (props) => {
     const [fruitDetails, setFruitDetails] = useState(null)
-    // const [fruitImage, setFruitImage] = useState(null)
-
 
     useEffect (() => {
         const getDetails = async () => {
@@ -14,19 +12,6 @@ const FruitDetails = (props) => {
         }
         getDetails()
     }, [props.selectedFruit])
-
-        // const getImage = async () => {
-        //     const imgResponse = await axios.get('https://api.unsplash.com/photos/random', {
-        //         headers: {
-        //             'Authorization': 'Client-ID j3RR6Cz3D1jpI66zyMOB0sWQQuTT8kZru1IoOIl0ALM',
-        //             'Query': 'Banana',
-        //             'Count': 1
-        //           } 
-        //     })
-        //     console.log(imgResponse)
-        //     setFruitImage(imgResponse)
-        // }
-        // getImage()
     
     return (
         <div>
@@ -37,13 +22,12 @@ const FruitDetails = (props) => {
                     <div className='imageDiv'>
                     <img src={`https://source.unsplash.com/random/300×300/?${fruitDetails.name}+'fruit'/`} alt = 'Fruit'/>
                     </div>
-                    {/* <img className='photo' src={fruitImage} alt='Selected Fruit'/> */}
                             <h2>Taxonomy</h2>
                             <p>Family : {fruitDetails.family}</p>
                             <p>Genus : {fruitDetails.genus}</p>
                             <p>Order : {fruitDetails.order}</p>
                             <h2>Nutrition Information</h2>
-                         <p>Carbohydrates : {fruitDetails.nutritions.carbohydrates}</p>   
+                        <p>Carbohydrates : {fruitDetails.nutritions.carbohydrates}</p>   
                         <p>Protein : {fruitDetails.nutritions.protein}</p>
                         <p>Fat : {fruitDetails.nutritions.fat}</p>
                         <p>Calories : {fruitDetails.nutritions.calories}</p>
--- my-app/src/globals.js
+++ my-app/src/globals.js
@@ -1,4 +1,3 @@
 export const BASE_URL = 'https://www.fruityvice.com/api/fruit/'
 
-export const API_KEY = process.env.REACT_APP_FRUITYVICE_KEY
 
